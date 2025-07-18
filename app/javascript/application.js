daeb7db4d1f3bce0efe5bc61ed8974afd80bbe3e
Rafael Moncores <rafamoncores@gmail.com>
2025-07-18 13:50:48 -0300

    feat: add working flashes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,28 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import * as Popper from "@popperjs/core";
+window.Popper = Popper;
+
 import "bootstrap"
+
+document.addEventListener("turbo:load", function() {
+  const closeButtons = document.querySelectorAll('.message .close');
+
+  closeButtons.forEach(button => {
+    button.addEventListener('click', function() {
+      const message = this.closest('.message');
+      if (message) {
+        // Adiciona uma classe para animar o fade-out e depois remover
+        message.style.opacity = 0;
+        message.style.transition = 'opacity 0.5s ease-out';
+
+        // Remove a mensagem do DOM após a transição
+        setTimeout(() => {
+          message.remove();
+        }, 500); // Tempo igual à duração da transição
+      }
+    });
+  });
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,11 +1,8 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.3/lib/index.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "@popperjs/core", to: "popper.js", preload: true
+pin "@popperjs/core", to: "@popperjs/core/dist/umd/popper.js", preload: true
+pin "bootstrap", to: "bootstrap/dist/js/bootstrap.min.js", preload: true
