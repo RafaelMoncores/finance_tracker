2bc04d6b6cf558b179b1703c3a59f1b5f9ef06cb
Rafael Moncores <rafamoncores@gmail.com>
2025-07-21 11:36:32 -0300

    display stock price in browser

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   get "my_portfolio", to: "users#my_portfolio"
+  get "search_stock", to: "stocks#search"
   get "up" => "rails/health#show", as: :rails_health_check
 end
