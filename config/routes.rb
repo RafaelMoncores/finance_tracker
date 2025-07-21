dd77c8503ed98c98605ac8435bd2715fa59e2a88
Rafael Moncores <rafamoncores@gmail.com>
2025-07-21 10:22:21 -0300

    feat: add front-end setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
+  get "my_portfolio", to: "users#my_portfolio"
   get "up" => "rails/health#show", as: :rails_health_check
 end
