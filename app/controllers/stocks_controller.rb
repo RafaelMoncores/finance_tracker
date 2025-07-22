fc39bb76cb4e2c138db137ba298b2d52e5633aa4
Rafael Moncores <rafamoncores@gmail.com>
2025-07-22 08:40:06 -0300

    fix: remove ajax to make it work

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,16 +2,16 @@ class StocksController < ApplicationController
   def search
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
+
       if @stock && @stock.valid?
-        respond_to do |format|
-          format.js { render partial: "users/result" }
-        end
+        flash.now[:notice] = "Stock '#{@stock.ticker}' found successfully!"
+        render "users/my_portfolio"
       else
-        flash[:alert] = "Stock not found. Please check the symbol and try again."
+        flash[:alert] = "Could not get data for '#{params[:stock]}'. Please check the symbol and try again (API limit might be reached)."
         redirect_to my_portfolio_path
       end
     else
-      flash[:alert] = "Please enter a symbol to search"
+      flash[:alert] = "Please enter a symbol to search."
       redirect_to my_portfolio_path
     end
   end
