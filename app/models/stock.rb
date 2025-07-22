70b2ebd7c7d55502b9e9ce54058bea185e1a46eb
Rafael Moncores <rafamoncores@gmail.com>
2025-07-22 17:00:02 -0300

    feat: add funcionality to track stocks from front-end

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -40,4 +40,8 @@ class Stock < ApplicationRecord
       nil # Retorna nil em caso de exceção geral
     end
   end
+
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
 end
