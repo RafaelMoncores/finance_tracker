9cbe7221810b5a916f442babf7e632c168e9a65e
Rafael Moncores <rafamoncores@gmail.com>
2025-07-23 11:10:38 -0300

    feat: add first and last name to sign up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name, :last_name ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :first_name, :last_name ])
   end
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
